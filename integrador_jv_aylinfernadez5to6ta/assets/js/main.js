4628a2052dc43254e884debf82a4085ffd16deb2
aylin <fernandezaylin614qgmail.com>
2023-06-28 00:16:49 -0300

    ya fue agregado lo que falta

---
+++ integrador_jv_aylinfernadez5to6ta/assets/js/main.js
@@ -0,0 +1,10 @@
+class Productos{
+    constructor (nombre,precio){
+      this.nombre=nombre;
+      this.precio = precio;
+    }
+}
+let ListaProductos=[
+    {nombre: "Chocolate feltford", precio: 12 },
+];
+ListaProductos.unshift(ListaProductos);
