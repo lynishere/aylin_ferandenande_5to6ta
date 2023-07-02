a9e569a2f19ae069e67d6ea2def9cb8328f7d8bf
aylin <fernandezaylin614qgmail.com>
2023-07-02 19:31:02 -0300

    actualzaciones y parte dos

--- integrador_jv_aylinfernadez5to6ta/assets/js/main.js
+++ integrador_jv_aylinfernadez5to6ta/assets/js/main.js
@@ -1,10 +1,30 @@
-class Productos{
+let productos=[]; //es el vector o array de productos
+class Producto{
     constructor (nombre,precio){
       this.nombre=nombre;
       this.precio = precio;
     }
 }
-let ListaProductos=[
-    {nombre: "Chocolate feltford", precio: 12 },
-];
-ListaProductos.unshift(ListaProductos);
+function AgregarProducto(evento) {
+  evento.preventDefault();
+  const nombre = document.getElementById("producto-nombre").value;
+  const precio = document.getElementById("producto-precio").value;
+
+  if (nombre.trim() !== "" && !isNaN(parseFloat(precio))) {
+    const ProductoNuevo = new Producto(nombre, precio);
+    productos.push(ProductoNuevo);
+    const ListaProductos = document.getElementById("lista-productos");
+    const li = document.createElement("li");
+     li.textContent = `Nombre: ${ProductoNuevo.nombre}, Precio: $${ProductoNuevo.precio}`;
+     ListaProductos.appendChild(li);
+     document.getElementById("producto-nombre").value = "";
+    document.getElementById("producto-precio").value = "";
+  }else {
+    alert("Ingrese valores válidos para el nombre y el precio.");
+}
+}
+const FormularioProducto = document.getElementById("formulario-producto");
+FormularioProducto.addEventListener( "submit", AgregarProducto);
+
+
+
